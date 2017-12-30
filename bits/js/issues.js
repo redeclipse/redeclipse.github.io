94c8f8a6c01bc0b06d9e8aad6d8688c898369cab
Quinton Reeves <qreeves@gmail.com>
2017-12-30 15:47:44 +1000

    fix alignment and add links to view/reply

--- bits/js/issues.js
+++ bits/js/issues.js
@@ -29,6 +29,7 @@ Element.prototype.makechild = function(elemname, idname, classname) {
 // Issues API
 var issues_page = 0;
 var issues_data = null;
+var issues_current = null;
 var issues_comments_data = null;
 
 function issues_get()
@@ -52,7 +53,7 @@ function issues_create(item, iter) {
     var row = document.createElement('tr');
     row.id = 'issues-t-row';
     row.className = 'issues-' + (iter%2 ? 'bg1' : 'bg2');
-    row.innerHTML += '<td id="issues-t-number" class="issues-center"><a href="' + item.html_url + '" target="_blank">#' + item.number + '</a></td>';
+    row.innerHTML += '<td id="issues-t-number" class="issues-center"><a href="' + item.html_url + '#show_issue" target="_blank">#' + item.number + '</a></td>';
     var title = document.createElement('td');
     title.id = 'issues-t-title';
     title.className = 'issues-left';
@@ -70,33 +71,44 @@ function issues_create(item, iter) {
 }
 
 function issues_view(item, hbody, hrow) {
-    var head = hrow.makechild('th', 'issues-h-comments-info', 'issues-left');
-    head.innerHTML = ' <a href="' + item.html_url + '" target="_blank">#' + item.number + ': ' + item.title + '</a>';
-    head.innerHTML += ' updated <time class="timeago" datetime="' + item.updated_at + '">' + issues_date(item.updated_at) + '</time>';
-    head.innerHTML += '<span id="issues-h-comments-avatar"><a href="' + item.user.html_url + '" target="_blank">' + item.user.login + ' <img src="' + item.user.avatar_url + '" alt="' + item.user.login + '" /></a></span>';
+    var head = hrow.makechild('th', 'issues-h-comments-info', 'issues-left'),
+        span = head.makechild('span', 'issues-h-comments-span', 'issues-left issues-middle'),
+        avat = head.makechild('span', 'issues-h-comments-avatar', 'issues-right issues-middle');
+    span.innerHTML = ' <a href="' + item.html_url + '#show_issue" target="_blank">#' + item.number + ': ' + item.title + '</a>';
+    span.innerHTML += ' updated <time class="timeago" datetime="' + item.updated_at + '">' + issues_date(item.updated_at) + '</time>';
+    avat.innerHTML += '<a href="' + item.user.html_url + '" target="_blank">' + item.user.login + ' <img src="' + item.user.avatar_url + '" alt="' + item.user.login + '" /></a>';
     var irow = hbody.makechild('tr', 'issues-t-comments-row', ''),
-        info = irow.makechild('td', 'issues-t-comments-info', 'issues-left');
-    info.innerHTML = sdconv.makeHtml(item.body);
-    issues_script_comments(pagedata.comments != null ? pagedata.comments : item.comments_url + '?callback=issues_comments');
+        info = irow.makechild('td', 'issues-t-comments-info', 'issues-left'),
+        cont = info.makechild('span', 'issues-t-comments-span', 'issues-left');
+    cont.innerHTML = sdconv.makeHtml(item.body);
+    issues_script_comments(pagedata.comments != null ? pagedata.comments : item.comments_url + '?callback=issues_comments', item);
 }
 
 function issues_view_comment(item, comment, hbody) {
     var hrow = hbody.makechild('tr', 'issues-h-comments-row', ''),
-        head = hrow.makechild('th', 'issues-h-comments-info', 'issues-left');
-    head.innerHTML = ' <a href="' + item.html_url + '" target="_blank">comment #' + comment + '</a>';
-    head.innerHTML += ' updated <time class="timeago" datetime="' + item.updated_at + '">' + issues_date(item.updated_at) + '</time>';
-    head.innerHTML += '<span id="issues-h-comments-avatar"><a href="' + item.user.html_url + '" target="_blank">' + item.user.login + ' <img src="' + item.user.avatar_url + '" alt="' + item.user.login + '" /></a></span>';
+        head = hrow.makechild('th', 'issues-h-comments-info', 'issues-left'),
+        span = head.makechild('span', 'issues-h-comments-span', 'issues-left issues-middle'),
+        avat = head.makechild('span', 'issues-h-comments-avatar', 'issues-right issues-middle');
+    span.innerHTML = ' <a href="' + item.html_url + '" target="_blank">comment #' + comment + '</a>';
+    span.innerHTML += ' updated <time class="timeago" datetime="' + item.updated_at + '">' + issues_date(item.updated_at) + '</time>';
+    avat.innerHTML += '<a href="' + item.user.html_url + '" target="_blank">' + item.user.login + ' <img src="' + item.user.avatar_url + '" alt="' + item.user.login + '" /></a>';
     var irow = hbody.makechild('tr', 'issues-t-comments-row', ''),
-        info = irow.makechild('td', 'issues-t-comments-info', 'issues-left');
-    info.innerHTML = sdconv.makeHtml(item.body);
+        info = irow.makechild('td', 'issues-t-comments-info', 'issues-left'),
+        cont = info.makechild('span', 'issues-t-comments-span', 'issues-left');
+    cont.innerHTML = sdconv.makeHtml(item.body);
 }
 
 function issues_build_comments() {
-    if(issues_page <= 0) return;
+    if(issues_page <= 0 || issues_current == null) return;
     var hbody = document.getElementById('issues-body');
     for(var i = 0; i < issues_comments_data.length; i++) {
         issues_view_comment(issues_comments_data[i], i+1, hbody);
     }
+    var hrow = hbody.makechild('tr', 'issues-t-reply-row', ''),
+        head = hrow.makechild('td', 'issues-t-reply-info', 'issues-center'),
+        span = head.makechild('span', 'issues-t-reply-span', 'issues-middle');
+    span.innerHTML = '<a href="' + issues_current.html_url + '#show_issue" target="_blank">View on GitHub</a>'
+    span.innerHTML += ' | <a href="' + issues_current.html_url + '#partial-timeline-marker" target="_blank">Reply on GitHub</a>';
     jQuery("time.timeago").timeago();
 }
 
@@ -119,6 +131,7 @@ function issues_build() {
         hbody.innerHTML = '';
         for(var i = 0; i < issues_data.length; i++) {
             if(issues_data[i].number == issues_page) {
+                issues_current = issues_data[i];
                 issues_view(issues_data[i], hbody, hrow);
                 break;
             }
@@ -134,6 +147,7 @@ function issues_build() {
         hrow.innerHTML += '<th id="issues-h-updated" class="issues-center">Updated</th>';
         hbody.innerHTML = "";
         for(var i = 0; i < issues_data.length; i++) {
+            issues_current = issues_data[i];
             var row = issues_create(issues_data[i], i);
             hbody.appendChild(row);
         }
@@ -141,7 +155,7 @@ function issues_build() {
     jQuery("time.timeago").timeago();
 }
 
-function issues_script_comments(src)
+function issues_script_comments(src, item)
 {
     var issues_script = document.getElementById('issues-script-comment');
     if(issues_script != null) {
